045246d2da8cead089fd3068c365ec58356a1e46
ThucTran <thucspc@gmail.com>
2020-12-13 09:04:41 +0700

    2

--- ZUpdate.bat
+++ ZUpdate.bat
@@ -5,10 +5,10 @@ set /p commit=Enter Commit:
 echo %commit%
 
 
-call git pull origin master
+call git pull
 call git add .
 
 
 call git commit -m %date%_%time%_%commit%
-call git push origin master -f
+call git push origin main -f
 PAUSE
